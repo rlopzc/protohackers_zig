0d9bc0e48dc713cc86ed862415b1f0a0955d306e
Romario López <romariolopezc@gmail.com>
2024-10-14 20:49:45 -0600

    Simple echo TCP server

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,26 @@
 const std = @import("std");
+const log = std.log;
+const net = std.net;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const address = try net.Address.resolveIp("127.0.0.1", 3000);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var server = try address.listen(.{});
+    defer server.deinit();
+    log.info("Server listening on port {d}", .{address.getPort()});
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var buf: [1024]u8 = undefined;
+    while (true) {
+        var socket = try server.accept();
+        defer socket.stream.close();
 
-    try bw.flush(); // don't forget to flush!
+        log.info("Client {any} connected", .{socket.address});
+
+        const bytes_read = try socket.stream.read(&buf);
+        std.debug.print("bytes_read={d} buffer={s}\n", .{ bytes_read, buf });
+
+        try socket.stream.writeAll(&buf);
+    }
 }
 
 test "simple test" {
