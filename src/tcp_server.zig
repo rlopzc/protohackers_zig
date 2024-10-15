cadccf555baa04bf5cc8d123f82b1d20dee97042
Romario López <romariolopezc@gmail.com>
2024-10-15 11:50:34 -0600

    Bind to 0.0.0.0

--- src/tcp_server.zig
+++ src/tcp_server.zig
@@ -3,7 +3,7 @@ const log = std.log;
 const net = std.net;
 
 pub fn start(port: u16) !net.Server {
-    const address = try net.Address.resolveIp("127.0.0.1", port);
+    const address = try net.Address.resolveIp("0.0.0.0", port);
 
     const server = try address.listen(.{
         .reuse_address = true,
