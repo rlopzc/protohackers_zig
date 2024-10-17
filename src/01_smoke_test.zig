95182709b869fde8f07377ba03b66f2f3bf0b36b
Romario López <romariolopezc@gmail.com>
2024-10-17 08:28:45 -0600

    Improve tcp server writing only read bytes

--- src/01_smoke_test.zig
+++ src/01_smoke_test.zig
@@ -5,28 +5,41 @@ const net = std.net;
 const TcpServer = @import("tcp_server.zig");
 
 pub fn main() !void {
-    var server = try TcpServer.start(3000);
+    var server = TcpServer.start(3000) catch std.process.exit(1);
     defer server.deinit();
 
     while (true) {
-        const socket = try server.accept();
+        const socket = server.accept() catch |err| {
+            log.err("failed to accept conn err={}", .{err});
+            continue;
+        };
         const thread = try std.Thread.spawn(.{}, socket_loop, .{socket});
         thread.detach();
     }
 }
 
 fn socket_loop(socket: net.Server.Connection) !void {
-    log.info("Client {} connected", .{socket.address});
+    log.info("jlient {} connected", .{socket.address});
     defer socket.stream.close();
 
     var buf: [1024]u8 = undefined;
     while (true) {
-        const bytes_read = socket.stream.read(&buf) catch break;
-
+        const bytes_read = socket.stream.read(&buf) catch |err| {
+            log.err("client {} err while reading from socket error={}", .{ socket.address, err });
+            break;
+        };
         if (bytes_read == 0) break;
-        log.debug("Client {} send bytes={d} buffer={s}", .{ socket.address, bytes_read, buf });
 
-        try socket.stream.writeAll(&buf);
+        log.info("client {} send bytes={d} bytes={s}", .{
+            socket.address,
+            bytes_read,
+            buf[0..(bytes_read - 1)],
+        });
+
+        _ = socket.stream.writeAll(buf[0..bytes_read]) catch |err| {
+            log.err("client {} err while writing to socket error={}", .{ socket.address, err });
+            break;
+        };
         buf = undefined;
     }
 
