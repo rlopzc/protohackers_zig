fd243e375e399af01f04e4492cf6c7b7d44efff1
Romario López <romariolopezc@gmail.com>
2024-10-23 15:14:42 -0600

    Improve Client

--- src/client.zig
+++ src/client.zig
@@ -1,36 +1,46 @@
 const std = @import("std");
 const log = std.log.scoped(.client);
 const net = std.net;
+const mem = std.mem;
 
 pub const Client = struct {
+    allocator: mem.Allocator,
     socket: net.Server.Connection,
+    buffer: []u8,
+
+    const Self = @This();
 
     const callback = fn (msg: []const u8, socket: *const net.Server.Connection) ?Action;
     pub const Action = enum {
         close_conn,
     };
 
-    pub fn run(self: Client, callback_fn: callback) !void {
+    pub fn new(allocator: mem.Allocator, socket: net.Server.Connection) !Self {
+        return .{
+            .allocator = allocator,
+            .socket = socket,
+            .buffer = try allocator.alloc(u8, 1024),
+        };
+    }
+
+    fn read(self: Self) !?[]u8 {
+        const value = try self.socket.stream.reader().readUntilDelimiterOrEof(self.buffer, '\n');
+        return value;
+    }
+
+    fn deinit(self: Self) void {
+        self.allocator.free(self.buffer);
+        self.socket.stream.close();
+    }
+
+    pub fn run(self: Self, callback_fn: callback) !void {
+        defer self.deinit();
         log.info("client {} connected", .{self.socket.address});
-        defer self.socket.stream.close();
-
-        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        defer _ = gpa.deinit();
-
-        var buffer: [1024]u8 = undefined;
 
         while (true) {
-            defer buffer = undefined;
-            const bytes_read = try self.socket.stream.read(&buffer);
-            if (bytes_read == 0) break;
+            const value = try self.read() orelse break;
 
-            log.info("client {} sent bytes={} buffer={}", .{
-                self.socket.address,
-                bytes_read,
-                std.zig.fmtEscapes(buffer[0..bytes_read]),
-            });
-
-            if (callback_fn(buffer[0..bytes_read], &self.socket)) |action| switch (action) {
+            if (callback_fn(value, &self.socket)) |action| switch (action) {
                 .close_conn => {
                     break;
                 },
