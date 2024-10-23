5cc1cbe5d17dc7939668471852dfefc64530d10a
Romario López <romariolopezc@gmail.com>
2024-10-23 15:14:31 -0600

    Improve TcpServer

--- src/tcp_server.zig
+++ src/tcp_server.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
 const log = std.log.scoped(.tcp_server);
 const net = std.net;
+const mem = std.mem;
 
 const Client = @import("client.zig").Client;
 
 pub const TcpServer = struct {
+    allocator: mem.Allocator,
     server: net.Server,
 
-    pub fn start(port: u16) !TcpServer {
+    const Self = @This();
+
+    pub fn start(allocator: mem.Allocator, port: u16) !Self {
         const address = try net.Address.resolveIp("0.0.0.0", port);
 
         const server = try address.listen(.{
@@ -15,15 +19,18 @@ pub const TcpServer = struct {
         });
         log.info("Server listening on port {d}", .{address.getPort()});
 
-        return TcpServer{ .server = server };
+        return .{
+            .allocator = allocator,
+            .server = server,
+        };
     }
 
-    pub fn accept(self: *TcpServer) !Client {
+    pub fn accept(self: *Self) !Client {
         const socket = try self.server.accept();
-        return Client{ .socket = socket };
+        return Client.new(self.allocator, socket);
     }
 
-    pub fn deinit(self: *TcpServer) void {
+    pub fn deinit(self: *Self) void {
         self.server.deinit();
     }
 };
