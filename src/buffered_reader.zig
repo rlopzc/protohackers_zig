93ba5048c208689c3e8c73cf9718b091a5290259
Romario López <romariolopezc@gmail.com>
2024-11-29 08:25:17 -0600

    Move reader outside of client

---
+++ src/buffered_reader.zig
@@ -0,0 +1,89 @@
+const std = @import("std");
+const log = std.log.scoped(.buffered_reader);
+const net = std.net;
+const mem = std.mem;
+
+pub const Reader = struct {
+    buf: []u8,
+    pos: usize = 0,
+    start: usize = 0,
+    stream: net.Stream,
+    delimiterFinderFn: DelimiterFinder,
+
+    const Self = @This();
+    pub const DelimiterFinder = *const fn (unprocessed: []u8) ?usize;
+
+    pub fn readMessage(self: *Self) ![]u8 {
+        var buf = self.buf;
+        // check if there is a message in my buffer, return it
+        // if there is no message, read from the socket and leave it unprocessed
+        while (true) {
+            if (try self.bufferedMessage()) |msg| {
+                return msg;
+            }
+
+            const pos = self.pos;
+            const bytes_read = try self.stream.read(buf[pos..]);
+            if (bytes_read == 0) {
+                // If we consumed every byte and there are no pending buffer items, return error.Closed
+                if (self.start == self.pos) {
+                    return error.Closed;
+                } else {
+                    // Otherwise, return the pending items in the buffer
+                    const unprocessed = buf[self.start..self.pos];
+                    self.pos = self.start;
+                    return unprocessed;
+                }
+            }
+
+            self.pos = pos + bytes_read;
+        }
+    }
+
+    // Checks if there's a full message in self.buf already.
+    // If there isn't, checks that we have enough spare space in self.buf for
+    // the next message.
+    fn bufferedMessage(self: *Self) !?[]u8 {
+        const buf = self.buf;
+        // position up to where there's valid data
+        const pos = self.pos;
+        // start of the next valid message
+        const start = self.start;
+
+        std.debug.assert(pos >= start);
+        const unprocessed = buf[start..pos];
+
+        // search index of the delimiter
+        const delimiter_index = self.delimiterFinder(unprocessed);
+        if (delimiter_index == null) {
+            self.ensureSpace(128) catch unreachable;
+            return null;
+        }
+
+        const delimiter_pos = delimiter_index.? + 1;
+        self.start = start + delimiter_pos;
+
+        return unprocessed[0..delimiter_pos];
+    }
+
+    fn ensureSpace(self: *Self, space: usize) error{BufferTooSmall}!void {
+        const buf = self.buf;
+        if (buf.len < space) {
+            log.err("buffer to small, space={d}", .{space});
+            return error.BufferTooSmall;
+        }
+
+        const start = self.start;
+        const spare = buf.len - start;
+        if (spare >= space) {
+            return;
+        }
+
+        const pos = self.pos;
+        const unprocessed = buf[start..pos];
+        std.mem.copyForwards(u8, buf[0..unprocessed.len], unprocessed);
+
+        self.start = 0;
+        self.pos = unprocessed.len;
+    }
+};
--- src/client.zig
+++ src/client.zig
@@ -2,89 +2,7 @@ const std = @import("std");
 const log = std.log.scoped(.client);
 const net = std.net;
 const mem = std.mem;
-
-const Reader = struct {
-    buf: []u8,
-    pos: usize = 0,
-    start: usize = 0,
-    stream: net.Stream,
-
-    const Self = @This();
-
-    fn readMessage(self: *Self) ![]u8 {
-        var buf = self.buf;
-        // check if there is a message in my buffer, return it
-        // if there is no message, read from the socket and leave it unprocessed
-        while (true) {
-            if (try self.bufferedMessage()) |msg| {
-                return msg;
-            }
-
-            const pos = self.pos;
-            const bytes_read = try self.stream.read(buf[pos..]);
-            if (bytes_read == 0) {
-                // If we consumed every byte and there are no pending buffer items, return error.Closed
-                if (self.start == self.pos) {
-                    return error.Closed;
-                } else {
-                    // Otherwise, return the pending items in the buffer
-                    const unprocessed = buf[self.start..self.pos];
-                    self.pos = self.start;
-                    return unprocessed;
-                }
-            }
-
-            self.pos = pos + bytes_read;
-        }
-    }
-
-    // Checks if there's a full message in self.buf already.
-    // If there isn't, checks that we have enough spare space in self.buf for
-    // the next message.
-    fn bufferedMessage(self: *Self) !?[]u8 {
-        const buf = self.buf;
-        // position up to where there's valid data
-        const pos = self.pos;
-        // start of the next valid message
-        const start = self.start;
-
-        std.debug.assert(pos >= start);
-        const unprocessed = buf[start..pos];
-
-        // search index of the delimiter
-        const delimiter_index = std.mem.indexOfScalar(u8, unprocessed, '\n');
-        if (delimiter_index == null) {
-            self.ensureSpace(128) catch unreachable;
-            return null;
-        }
-
-        const delimiter_pos = delimiter_index.? + 1;
-        self.start = start + delimiter_pos;
-
-        return unprocessed[0..delimiter_pos];
-    }
-
-    fn ensureSpace(self: *Self, space: usize) error{BufferTooSmall}!void {
-        const buf = self.buf;
-        if (buf.len < space) {
-            log.err("buffer to small, space={d}", .{space});
-            return error.BufferTooSmall;
-        }
-
-        const start = self.start;
-        const spare = buf.len - start;
-        if (spare >= space) {
-            return;
-        }
-
-        const pos = self.pos;
-        const unprocessed = buf[start..pos];
-        std.mem.copyForwards(u8, buf[0..unprocessed.len], unprocessed);
-
-        self.start = 0;
-        self.pos = unprocessed.len;
-    }
-};
+const Reader = @import("./buffered_reader.zig").Reader;
 
 pub const Client = struct {
     socket: net.Server.Connection,
@@ -108,12 +26,21 @@ pub const Client = struct {
         self.socket.stream.close();
     }
 
-    pub fn run(self: Self, callback_fn: callback) !void {
+    pub fn run(
+        self: Self,
+        callback_fn: callback,
+        delimiterFinder: Reader.DelimiterFinder,
+    ) !void {
         defer self.deinit();
         log.info("client {} connected", .{self.socket.address});
 
         var buf: [4096]u8 = undefined;
-        var reader = Reader{ .pos = 0, .buf = &buf, .stream = self.socket.stream };
+        var reader = Reader{
+            .pos = 0,
+            .buf = &buf,
+            .stream = self.socket.stream,
+            .delimiterFinder = delimiterFinder,
+        };
 
         while (true) {
             const msg = reader.readMessage() catch break;
