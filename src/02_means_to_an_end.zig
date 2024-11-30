bfdb720e065c08df2384cbbc98cc6d7932fe69c9
Romario López <romariolopezc@gmail.com>
2024-11-30 10:07:53 -0600

    Add prices hashmap and insert op

--- src/02_means_to_an_end.zig
+++ src/02_means_to_an_end.zig
@@ -6,9 +6,15 @@ const mem = std.mem;
 const TcpServer = @import("tcp_server.zig").TcpServer;
 const Client = @import("client.zig").Client;
 
+var prices: std.HashMap(i32, i32, std.hash_map.AutoContext(i32), std.hash_map.default_max_load_percentage) = undefined;
+
 pub fn main() !void {
     var server = TcpServer.start(3000) catch std.process.exit(1);
     defer server.deinit();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    prices = std.AutoHashMap(i32, i32).init(gpa.allocator());
+    defer prices.deinit();
 
     while (true) {
         const client = server.accept() catch |err| {
@@ -37,13 +43,44 @@ fn delimiterFinder(unprocessed: []u8) ?usize {
     }
 }
 
+// TODO: shared memory between threads.
+// Lock mechanism?
 fn callback(msg: []const u8, client: *const Client) !void {
     std.debug.print("bin={b}\nhex={x}\n", .{ msg, msg });
+    // The first byte of a message is a character indicating its type. This will be
+    // an ASCII uppercase 'I' or 'Q' character, indicating whether the message
+    // inserts or queries prices, respectively.
     const op: u8 = msg[0];
-    const timestamp: i32 = mem.readInt(i32, msg[1..5], .big);
-    const price: i32 = mem.readInt(i32, msg[5..9], .big);
 
-    std.debug.print("op={} timestamp={d} price={d}\n", .{ op, timestamp, price });
+    // The next 8 bytes are two signed two's complement 32-bit integers in network
+    // byte order (big endian), whose meaning depends on the message type.
+    // We'll refer to these numbers as int32, but note this may differ from
+    // your system's native int32 type (if any), particularly with regard to
+    // byte order.
+    const first: i32 = mem.readInt(i32, msg[1..5], .big);
+    const second: i32 = mem.readInt(i32, msg[5..9], .big);
 
-    try client.write(msg);
+    std.debug.print("op={} first={d} second={d}\n", .{ op, first, second });
+
+    switch (op) {
+        'I' => {
+            // An insert message lets the client insert a timestamped price.
+            // The message format is:
+            // Byte:  |  0  |  1     2     3     4  |  5     6     7     8  |
+            // Type:  |char |         int32         |         int32         |
+            // Value: | 'I' |       timestamp       |         price         |
+            //
+            // The first int32 is the timestamp, in seconds since 00:00, 1st Jan 1970.
+            // The second int32 is the price, in pennies, of this client's asset, at the given timestamp.
+            const value = try prices.getOrPut(first);
+            if (!value.found_existing) {
+                value.value_ptr.* = second;
+            }
+            std.debug.print("{any}", .{prices});
+        },
+        'Q' => {
+            try client.write("kaka");
+        },
+        else => {},
+    }
 }
