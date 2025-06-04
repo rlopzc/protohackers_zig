b8d0f742c306b2cbef9fdf9606d74221916592e2
Romario López <romariolopezc@gmail.com>
2025-06-03 18:13:23 -0600

    Add log

--- src/udp_server.zig
+++ src/udp_server.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const log = std.log.scoped(.udp_server);
 const net = std.net;
 const posix = std.posix;
 
@@ -12,6 +13,7 @@ pub const UdpServer = struct {
     pub fn start(port: u16) !Self {
         const sock: posix.socket_t = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, posix.IPPROTO.UDP);
         const addr: net.Address = try net.Address.parseIp("0.0.0.0", port);
+        log.info("Server listening on port {d}", .{addr.getPort()});
 
         try posix.setsockopt(sock, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
         try posix.bind(sock, &addr.any, addr.getOsSockLen());
